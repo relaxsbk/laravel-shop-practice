e74633769a9682034e39145fde2538fa9965061d
relaxsbk.exe <pah4n1989@gmail.com>
2024-02-04 03:20:18 +0800

    настройка шаблонов

---
+++ resources/views/templates/main.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <script src="{{asset('assets/js/bootstrap.min.js')}}" defer></script>
+    <title>Пока шаблон</title>
+</head>
+<body>
+<header>
+    <div class="navbar">
+        @include('components.navbar')
+    </div>
+</header>
+
+<main>
+    <div class="main">
+        @yield('main')
+    </div>
+</main>
+</body>
+</html>
