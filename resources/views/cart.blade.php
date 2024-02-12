0d6c5fda29a46364882815c4cd890067b1954b72
relaxsbk.exe <pah4n1989@gmail.com>
2024-02-12 17:55:46 +0800

    Корзина - готов адаптив

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -5,75 +5,17 @@
 
 
     <div class="container mt-5">
-{{--        <h2 class="mb-4">Корзина</h2>--}}
-{{--        <div class="d-flex flex-wrap justify-content-between align-items-start">--}}
+        <nav class="fs-6 mt-4 mb-4" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{route('HomePage')}}">ГиперКвант</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Корзина</li>
+            </ol>
+        </nav>
 
-{{--            <div class="card text-dark bg-light mb-3" style="max-width: calc(100% - 24rem - 16px);">--}}
-{{--                <div class="card-body row align-items-center">--}}
-{{--                    <div class="col-auto">--}}
-{{--                        <img src="https://via.placeholder.com/100" alt="Product Image" class="img-fluid">--}}
-{{--                    </div>--}}
-{{--                    <div class="col">--}}
-{{--                        <h5 class="card-title mb-0">Название продукта с очень длинным названием, которое может занимать несколько строк</h5>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-auto mr-3">--}}
-{{--                        <p class="card-text mb-0">$50</p>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-auto mr-3">--}}
-{{--                        <label>--}}
-{{--                            <input type="number" value="1" class="form-control ">--}}
-{{--                        </label>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-auto">--}}
-{{--                        <button type="button" class="btn btn-danger">Удалить</button>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--            <div class="card text-dark bg-light mb-3" style="max-width: calc(100% - 24rem - 16px);">--}}
-{{--                <div class="card-body row align-items-center">--}}
-{{--                    <div class="col-auto">--}}
-{{--                        <img src="https://via.placeholder.com/100" alt="Product Image" class="img-fluid">--}}
-{{--                    </div>--}}
-{{--                    <div class="col">--}}
-{{--                        <h5 class="card-title mb-0">Название продукта с очень длинным названием, которое может занимать несколько строк</h5>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-auto mr-3">--}}
-{{--                        <p class="card-text mb-0">$50</p>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-auto mr-3">--}}
-{{--                        <label>--}}
-{{--                            <input type="number" value="1" class="form-control ">--}}
-{{--                        </label>--}}
-{{--                    </div>--}}
-{{--                    <div class="col-auto">--}}
-{{--                        <button type="button" class="btn btn-danger">Удалить</button>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
+        <h2 class="mb-4">Корзина</h2>
 
-
-
-
-{{--            <div class="card text-dark bg-light mb-3" style="width: 24rem;">--}}
-{{--                <div class="card-body">--}}
-{{--                    <h5 class="card-title">Итого</h5>--}}
-{{--                    <p class="card-text">Товары: $50</p>--}}
-{{--                    <p class="card-text">Стоимость доставки: $10</p>--}}
-{{--                    <h6 class="card-text">Итоговая сумма: $60</h6>--}}
-{{--                    <div class="form-check mb-3">--}}
-{{--                        <input class="form-check-input" type="checkbox" value="" id="agreeCheckbox">--}}
-{{--                        <label class="form-check-label" for="agreeCheckbox">--}}
-{{--                            Я согласен с условиями использования--}}
-{{--                        </label>--}}
-{{--                    </div>--}}
-{{--                    <button class="btn btn-primary btn-block" id="orderButton" disabled>Оформить заказ</button>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-
-        <div class="d-flex justify-content-between">
-            <div class="w-100 me-5">
+        <div class="row">
+            <div class="col-lg-8">
                 <!-- Карточки товаров -->
                 <div class="card bg-white border mb-3" style="max-width: 100%;">
                     <div class="card-body row align-items-center">
@@ -83,7 +25,6 @@
                         <div class="col">
                             <h5 class="card-title mb-0">Название продукта с очень длинным названием, которое может занимать несколько строк</h5>
                         </div>
-
                         <div class="col-auto mr-3">
                             <label>
                                 <input type="number" value="1" class="form-control ">
@@ -97,13 +38,13 @@
                         </div>
                     </div>
                 </div>
-
-                <!-- Добавьте здесь другие карточки товаров при необходимости -->
+                <!--  другие карточки  -->
             </div>
-            <div class="ml-auto">
+
+            <div class="col-lg-4">
                 <!-- Блок с итогом -->
-                <div class="card mb-3" style="width: 24rem; background-color: #DEF9F9;">
-                    <div class="card-body d-flex flex-column">
+                <div class="card mb-3" style="background-color: #DEF9F9;">
+                    <div class="card-body">
                         <h5 class="card-title fw-bold fs-3">Итого</h5>
                         <div class="d-flex justify-content-between align-items-center mb-2">
                             <span class="card-text text-secondary">Товаров на сумму:</span>
@@ -128,14 +69,8 @@
                         <button class="btn btn-primary text-white btn-block" id="orderButton" disabled>Оформить заказ</button>
                     </div>
                 </div>
-
             </div>
         </div>
-
-
-
-
-
     </div>
 
 
